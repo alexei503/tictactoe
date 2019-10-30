d96aaf3ec628ee82f7af7ec5ad283f3c26260b19
Daniel Barrera <you@example.com>
2019-10-30 11:38:32 -0600

    Updates

---
+++ precache-manifest.c47dedc1d97b98f208ad59f75fc3a845.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7f53067886a6bd2e411ebcd98707a097",
+    "url": "alexei503.github.io/tictactoe/index.html"
+  },
+  {
+    "revision": "f1031bb6845ba60f8c19",
+    "url": "alexei503.github.io/tictactoe/static/css/main.621c92a7.chunk.css"
+  },
+  {
+    "revision": "5fc33aed0e7becc2a37c",
+    "url": "alexei503.github.io/tictactoe/static/js/2.52dd166e.chunk.js"
+  },
+  {
+    "revision": "f1031bb6845ba60f8c19",
+    "url": "alexei503.github.io/tictactoe/static/js/main.0ac6f443.chunk.js"
+  },
+  {
+    "revision": "bea8fa7fe9065b66faa6",
+    "url": "alexei503.github.io/tictactoe/static/js/runtime-main.3b92fb2e.js"
+  }
+]);
